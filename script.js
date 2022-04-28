4d9c886a04818343b6bdcb1134d448aa0c20ce46
Martin Pluhar <pluhar.martin@outlook.com>
2022-04-28 23:09:38 +0200

    prettier reconfigured

--- script.js
+++ script.js
@@ -6,28 +6,30 @@ document.querySelector('.number').textContent = secretNumber;
 //hendler function
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ Není to číslo!!';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Správné číslo!';
-  } else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Moc vysoké 📈';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💔 Prohrál jsi 😥';
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔️ Není to číslo!!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Správné číslo!';
+    } else if (guess > secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'Moc vysoké 📈';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                '💔 Prohrál jsi 😥';
+        }
+    } else if (guess < secretNumber) {
+        if (score > 0) {
+            document.querySelector('.message').textContent = 'Moc nízké 📉';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                '💔 Prohrál jsi 😥';
+        }
     }
-  } else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Moc nízké 📉';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💔 Prohrál jsi 😥';
-    }
-  }
 });
